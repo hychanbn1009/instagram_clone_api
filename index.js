8f2eb56ba2eff630ee5fd8591551d7ce383c2b6a
hychanbn1009 <hychanbn1009@gmail.com>
2023-01-09 00:15:16 +0800

    Relocate socketio in index

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const path = require("path");
 const passport = require("passport");
 const LocalStrategy = require("passport-local").Strategy;
 const mongoose = require('mongoose');
+const Message = require("./src/models/Message");
 const bodyParser = require('body-parser');
 const cors = require('cors');
 const authRoutes = require("./src/routes/authRoutes");
@@ -12,9 +13,16 @@ const accountRoutes = require("./src/routes/accountRoutes");
 const commentRoutes = require("./src/routes/commentRoutes");
 const searchRoutes = require("./src/routes/searchRoutes");
 const messageRoutes = require("./src/routes/messageRouter");
+const { instrument } = require("@socket.io/admin-ui");
+const app = express();
+const server = require('http').createServer(app);
+const io = require('socket.io')(server, {
+  cors: {
+    origin: '*',
+  }
+});
 require('dotenv').config()
 
-const app = express();
 app.use(cors());
 
 // all json information pass first then run request handler
@@ -43,6 +51,34 @@ app.get("/",requireAuth, (req, res) => res.send("index"));
 
 app.set('port', (process.env.PORT || 5000));
 
-app.listen(app.get('port'), function() {
+server.listen(app.get('port'), function() {
     console.log("Node app is running at localhost:" + app.get('port'))
+});
+
+
+io.on('connection', socket=>{
+    socket.on("join-room",(room)=>{
+        {console.log(`join room ${room}`)}
+        socket.join(room)
+        Message.find({room:room})
+        .populate('sender', 'username')
+        .then(history=>{
+            {console.log(`emit chat history`)}
+            socket.emit('get-chat-history',history.reverse())
+        })
+    })
+    socket.on("send-message",async(message,room,userId)=>{
+        try{
+            {console.log(message)}
+            const msg = new Message({sender:userId,messageContent:message,room:room})
+            await msg.save()
+            socket.broadcast.to(room).emit('receive-message',message)
+        }catch(err){
+            console.log(err)
+        }
+    })
+})
+
+instrument(io, {
+    auth: false
 });
--- src/routes/messageRouter.js
+++ src/routes/messageRouter.js
@@ -4,39 +4,10 @@ const requireAuth = require('../middlewares/requireAuth');
 const Post = require('../models/Post');
 const User = require('../models/User');
 const Message = require("../models/Message");
-const { instrument } = require("@socket.io/admin-ui");
-const io = require ('socket.io')(process.env.PORT || 4000,{
-    cors:{
-        origin: "*"
-    }
-});
-
-module.exports = io.on('connection', socket=>{
-    socket.on("join-room",(room)=>{
-        {console.log(`join room ${room}`)}
-        socket.join(room)
-        Message.find({room:room})
-        .populate('sender', 'username')
-        .then(history=>{
-            {console.log(`emit chat history`)}
-            socket.emit('get-chat-history',history.reverse())
-        })
-    })
-    socket.on("send-message",async(message,room,userId)=>{
-        try{
-            {console.log(message)}
-            const msg = new Message({sender:userId,messageContent:message,room:room})
-            await msg.save()
-            socket.broadcast.to(room).emit('receive-message',message)
-        }catch(err){
-            console.log(err)
-        }
-    })
-})
-
-instrument(io, {
-    auth: false
-});
+const app = express();
+const http = require('http');
+const server = http.createServer(app);
+const { Server } = require("socket.io");
 
 const router = express.Router();
 
