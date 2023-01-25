fba07454cfb5d7fcfb254905825176cf39ab4862
hychanbn1009 <hychanbn1009@gmail.com>
2023-01-25 18:27:59 +0800

    Fixed searched user list return undefined

--- src/routes/searchRoutes.js
+++ src/routes/searchRoutes.js
@@ -11,7 +11,7 @@ router.use(requireAuth);
 router.post('/searchUser', async(req,res,next)=>{
     const {searchTarget} = req.body;
     try{
-        const searchedUserList = await User.find({username:{$regex:searchTarget}, $options: 'i'},{password:0})
+        const searchedUserList = await User.find({username:{$regex:searchTarget}},{fullname:1,username:1})
         res.status(200).send({searchedUserList});
     } catch(err){
         return res.status(422).send(err.message)
